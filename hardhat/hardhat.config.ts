6c1905c916b8ecc42e97b2e6f5bd1c0a15b2d2ee
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2024-02-01 16:01:40 +0100

    Move to main

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,58 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "dotenv/config";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+//const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+  },
+
+  defaultNetwork: "hardhat",
+  networks: {
+    // for mainnet
+    "lightlink-mainnet": {
+      url: "https://replicator.phoenix.lightlink.io/rpc/v1",
+      accounts: [PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    "lightlink-testnet": {
+      url: "https://replicator.pegasus.lightlink.io/rpc/v1",
+      accounts: [PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    "base-local": {
+      url: "http://localhost:8545",
+      accounts: [PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
+
+  etherscan: {
+    customChains: [
+      {
+        network: "pegasus",
+        chainId: 1891,
+        urls: {
+          apiURL: "https://pegasus.lightlink.io/api",
+          browserURL: "https://pegasus.lightlink.io",
+        },
+      },
+      {
+        network: "devnet",
+        chainId: 88,
+        urls: {
+          apiURL: "https://devnet.lightlink.io/api",
+          browserURL: "https://devnet.lightlink.io",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
---
+++ hardhat/test/easeTokenDrop.test.ts
@@ -0,0 +1,116 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+import { EaseTokenDrop } from "../typechain";
+
+describe("EaseTokenDrop", function () {
+  let owner: Signer,
+    recipient1: Signer,
+    recipient2: Signer,
+    easeTokenDrop: EaseTokenDrop;
+
+  beforeEach(async function () {
+    [owner, recipient1, recipient2] = await ethers.getSigners();
+
+    const EaseTokenDrop = await ethers.getContractFactory("EaseTokenDrop");
+    easeTokenDrop = (await EaseTokenDrop.deploy(
+      "EaseToken",
+      "EASE",
+      1000000
+    )) as EaseTokenDrop;
+    await easeTokenDrop.deployed();
+  });
+
+  it("Should deploy the contract with the correct initial state", async function () {
+    const totalSupply = await easeTokenDrop.totalSupply();
+    expect(totalSupply).to.equal(1000000 * 10 ** 18);
+
+    const ownerBalance = await easeTokenDrop.balanceOf(
+      await owner.getAddress()
+    );
+    expect(ownerBalance).to.equal(1000000 * 10 ** 18);
+  });
+
+  it("Should airdrop tokens to a list of addresses", async function () {
+    await easeTokenDrop
+      .connect(owner)
+      .doAirDrop(
+        [await recipient1.getAddress(), await recipient2.getAddress()],
+        1000
+      );
+
+    const recipient1Balance = await easeTokenDrop.balanceOf(
+      await recipient1.getAddress()
+    );
+    expect(recipient1Balance).to.equal(1000);
+
+    const recipient2Balance = await easeTokenDrop.balanceOf(
+      await recipient2.getAddress()
+    );
+    expect(recipient2Balance).to.equal(1000);
+  });
+
+  it("Should airdrop tokens with individual amounts to a list of addresses", async function () {
+    await easeTokenDrop
+      .connect(owner)
+      .sendBatch(
+        [await recipient1.getAddress(), await recipient2.getAddress()],
+        [500, 700]
+      );
+
+    const recipient1Balance = await easeTokenDrop.balanceOf(
+      await recipient1.getAddress()
+    );
+    expect(recipient1Balance).to.equal(500);
+
+    const recipient2Balance = await easeTokenDrop.balanceOf(
+      await recipient2.getAddress()
+    );
+    expect(recipient2Balance).to.equal(700);
+  });
+
+  it("Should transfer ETH balance to the owner", async function () {
+    const initialOwnerBalance = await ethers.provider.getBalance(
+      await owner.getAddress()
+    );
+
+    await (
+      await owner.sendTransaction({
+        to: easeTokenDrop.address,
+        value: ethers.utils.parseEther("1.0"),
+      })
+    ).wait();
+
+    await easeTokenDrop.connect(owner).transferEthToOnwer();
+
+    const finalOwnerBalance = await ethers.provider.getBalance(
+      await owner.getAddress()
+    );
+    expect(finalOwnerBalance.gt(initialOwnerBalance)).to.be.true;
+  });
+
+  it("Should self-destruct the contract and transfer remaining funds to the owner", async function () {
+    const initialOwnerBalance = await ethers.provider.getBalance(
+      await owner.getAddress()
+    );
+
+    await (
+      await owner.sendTransaction({
+        to: easeTokenDrop.address,
+        value: ethers.utils.parseEther("1.0"),
+      })
+    ).wait();
+
+    await easeTokenDrop.connect(owner).kill();
+
+    const finalOwnerBalance = await ethers.provider.getBalance(
+      await owner.getAddress()
+    );
+    expect(finalOwnerBalance.gt(initialOwnerBalance)).to.be.true;
+
+    // Attempting to interact with the contract after self-destruct should throw an error
+    await expect(easeTokenDrop.totalSupply()).to.be.revertedWith(
+      "Returned values aren't valid, did it run Out of Gas?"
+    );
+  });
+});
