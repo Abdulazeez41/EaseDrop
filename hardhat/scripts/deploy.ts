6c329ec43087ab08bc346423c326cde57f02b3df
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2024-02-02 20:22:28 +0100

    Modified deploy.js script for proper deployment

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,8 +1,21 @@
 import { ethers } from "hardhat";
+import {
+  EaseTokenDrop,
+  EaseTokenDrop__factory,
+  EaseNFTDrop,
+  EaseNFTDrop__factory,
+} from "../typechain-types";
 
 async function main() {
-  const easeTokenDrop = await ethers.getContractFactory("EaseTokenDrop");
-  const easedrop = await easeTokenDrop.deploy("Testtoken", "TKT", 2000);
+  const easeTokenDrop: EaseTokenDrop__factory =
+    (await ethers.getContractFactory(
+      "EaseTokenDrop"
+    )) as EaseTokenDrop__factory;
+  const easedrop: EaseTokenDrop = await easeTokenDrop.deploy(
+    "Testtoken",
+    "TKT",
+    2000
+  );
 
   await easedrop.deployed();
 
@@ -10,12 +23,14 @@ async function main() {
 
   console.log(`-------------------------------------------------------`);
 
-  const easeNFTDrop = await ethers.getContractFactory("EaseNFTDrop");
-  const easeDrop = await easeNFTDrop.deploy("Testtoken", "TKT");
+  const easeNFTDrop: EaseNFTDrop__factory = (await ethers.getContractFactory(
+    "EaseNFTDrop"
+  )) as EaseNFTDrop__factory;
+  const easeDrop: EaseNFTDrop = await easeNFTDrop.deploy("Testtoken", "TKT");
 
   await easeDrop.deployed();
 
-  console.log(`EaseTokenDrop deployed to ${easeDrop.address}`);
+  console.log(`EaseNFTDrop deployed to ${easeDrop.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
